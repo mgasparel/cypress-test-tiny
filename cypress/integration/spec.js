05ceb582387dc023585b71110392ff48f95a234f
Michael Gasparelli <mgasparelli@sircorp.com>
2020-02-19 21:40:40 -0500

    Simple repro that doesn't repro

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.server()
+    cy.route({ method: 'PUT', url: 'https://nocontentexample.azurewebsites.net/api/HttpTrigger1?code=P9h9uOLUo5EelWZX2B4A6TaVBEeyDAoIvwNlxqP1kIVQUba3qXPi9w==' }).as('nocontent');
+
+    cy.visit('./test.html')
+
+    cy.wait('@nocontent')
+      .should('have.property', 'status', 204);
   })
 })
